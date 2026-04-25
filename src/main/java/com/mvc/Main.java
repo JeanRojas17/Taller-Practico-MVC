7ce3307eedb114129d6c09e2efde5d2f0feb3c1c
Michael Lenis <lenismaicol9@gmail.com>
2026-04-25 11:27:12 -0500

    Agregando las funcionalidades de Materia

--- src/main/java/com/mvc/Main.java
+++ src/main/java/com/mvc/Main.java
@@ -2,12 +2,16 @@ package com.mvc;
 
 import com.mvc.controlador.ControladorDocente;
 import com.mvc.controlador.ControladorEstudiante;
+import com.mvc.controlador.ControladorMateria;
 import com.mvc.dao.DocenteDao;
 import com.mvc.dao.EstudianteDao;
+import com.mvc.dao.MateriaDao;
 import com.mvc.servicios.DocenteService;
 import com.mvc.servicios.EstudianteService;
+import com.mvc.servicios.MateriaService;
 import com.mvc.vista.VistaDocente;
 import com.mvc.vista.VistaEstudiante;
+import com.mvc.vista.VistaMateria;
 
 public class Main {
 
@@ -61,5 +65,28 @@ public class Main {
         // Flujo de eliminación de un docente por ID
         controladorDocente.eliminarDocente();
         controladorDocente.mostrarTodosLosDocentes();
+
+        // ── MATERIA ─────────────────────────────────────
+        VistaMateria vistaMateria = new VistaMateria();
+        MateriaDao materiaDao = new MateriaDao();
+        MateriaService materiaService = new MateriaService(materiaDao);
+        ControladorMateria controladorMateria = new ControladorMateria(vistaMateria, materiaService);
+
+        // Flujo de registro de una materia
+        controladorMateria.mostrarTodasLasMaterias();
+        controladorMateria.registrarMateria();
+        controladorMateria.mostrarTodasLasMaterias();
+
+        // Flujo de consulta de una materia por ID
+        int idMateria = vistaMateria.solicitarIdMateria();
+        controladorMateria.mostrarDetallesMateria(idMateria);
+
+        // Flujo de actualización de una materia por ID
+        controladorMateria.actualizarMateria();
+        controladorMateria.mostrarTodasLasMaterias();
+
+        // Flujo de eliminación de una materia por ID
+        controladorMateria.eliminarMateria();
+        controladorMateria.mostrarTodasLasMaterias();
     }
 }
